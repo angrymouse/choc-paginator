18f8e18a2fcca274913edb2fb4b744f5280f4eb2
anubra266 <anubra266@gmail.com>
2021-02-06 03:55:50 +0100

    Updates

---
+++ precache-manifest.6f857f18d13881118a523ca22f93c3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0283a591a3599269a897c209551d74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc9b774a0c5c5f7cd070",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "ef0f255d6937603969d4",
+    "url": "./static/js/2.b234b31e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "./static/js/2.b234b31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9b774a0c5c5f7cd070",
+    "url": "./static/js/main.c836337d.chunk.js"
+  },
+  {
+    "revision": "c33fdd4b819750f6ecc3",
+    "url": "./static/js/runtime-main.1276f6fb.js"
+  }
+]);
